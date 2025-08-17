9d0cccd376db5bc8eae13f4a839988ecf980e7c5
MidnytDeveloper <abhishek.sni101@gmail.com>
2025-08-17 13:46:52 +0530

    login form

--- src/Login.jsx
+++ src/Login.jsx
@@ -4,13 +4,23 @@ const Login = () => {
       <div className="flex justify-center my-10">
         <div className="card card-border bg-base-300 w-96">
           <div className="card-body">
-            <h2 className="card-title">Card Title</h2>
-            <p>
-              A card component has a figure, a body part, and inside body there
-              are title and actions parts
-            </p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+            <h2 className="card-title justify-center">Login</h2>
+            {/* Email ID */}
+            <div>
+              <fieldset className="fieldset my-2">
+                <legend className="fieldset-legend">Email ID</legend>
+                <input type="text" className="input" />
+              </fieldset>
+            </div>
+            {/* password */}
+            <div>
+              <fieldset className="fieldset my-2">
+                <legend className="fieldset-legend">Password</legend>
+                <input type="text" className="input" />
+              </fieldset>
+            </div>
+            <div className="card-actions justify-center my-2">
+              <button className="btn btn-primary">Login</button>
             </div>
           </div>
         </div>
